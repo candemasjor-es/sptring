32c511252a93533dfb3d9e3420a3a5606601c119
candemasjor_1987 <camilomascarelljorques@gmail.com>
2022-04-08 10:45:52 +0200

    nueva

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AccordionModule} from 'primeng/accordion';
+import { StudentsComponent } from './students/students.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { StudentSearchComponent } from './student-search/student-search.component';
+import { MessagesComponent } from './messages/messages.component';
+import { StudentDetailComponent } from './student-detail/student-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentsComponent,
+    DashboardComponent,
+    StudentSearchComponent,
+    MessagesComponent,
+    StudentDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AccordionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
